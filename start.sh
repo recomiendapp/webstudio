2a334436ada0f95201c984628932462e01c3b11b
Agustín Rodríguez <agustin.rodriguezortiz@orenyi.com>
2026-01-16 08:45:24 +0100

    resolveMigrations

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ fi
 # (2) Migraciones (PRODUCCIÓN)
 echo "Running database migrations (deploy mode)"
 pnpm --filter=@webstudio-is/prisma-client generate
-pnpm --filter=./packages/prisma-client migrations --dev --cwd ../../apps/builder
+pnpm --filter=./packages/prisma-client migrations migraate --dev ../../apps/builder
 
 # (2) Détecter l'entry serveur Remix : - priorité: build/server/**/index.js (Remix v2) - fallback: build/index.js (anciens templates)
 SSR_ENTRY=""
