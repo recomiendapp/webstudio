c5aac07655355dfa5599503c337abe7ed0a57ccf
Agustín Rodríguez <agustin.rodriguezortiz@orenyi.com>
2026-02-03 08:58:17 +0100

    fix

--- packages/project-build/src/db/build.ts
+++ packages/project-build/src/db/build.ts
@@ -266,6 +266,7 @@ export const createProductionBuild = async (
     project_id: props.projectId,
     deployment: JSON.stringify(props.deployment),
   });
+  console.log('createProductionBuild ', build);
   const buildId = build.data;
   if (build.error) {
     throw build.error;
