dbac3c7f689a40328c14c0969b9a10096c60ea32
Agustín Rodríguez <agustin.rodriguezortiz@orenyi.com>
2026-05-13 08:51:16 +0200

    fix cert for *.builderstaging

--- apps/builder/app/builder/features/topbar/add-domain.tsx
+++ apps/builder/app/builder/features/topbar/add-domain.tsx
@@ -52,7 +52,11 @@ export const AddDomain = ({
       const registrar = await nativeClient.domain.findDomainRegistrar.query({
         domain,
       });
- 
+      // enforce www subdomain when no support for cname flattening
+      // and root cname can conflict with MX or NS
+      //if (registrar.known && !registrar.cnameFlattening) {
+        //domain = `www.${domain}`;
+      //}
     }
 
     const result = await nativeClient.domain.create.mutate({
